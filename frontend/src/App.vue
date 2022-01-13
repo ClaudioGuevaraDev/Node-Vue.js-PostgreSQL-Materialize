6dc80d8bf5aec82998d74851bcd1c3c407e0f0f1
ClaudioGuevara <claudio.guevara.dev@gmail.com>
2022-01-13 19:21:11 -0300

    (frontend): Configuración inicial de webpack lista para correr vue.

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 class="title">App con Node y vue</h1>
+    </div>
+</template>
+
+<style scoped>
+
+.title {
+    color: red;
+    font-style: italic;
+}
+
+</style>
