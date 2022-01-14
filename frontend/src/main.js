2231ccf4b9e1272147c817b0bacd30a3df35879d
ClaudioGuevara <claudio.guevara.dev@gmail.com>
2022-01-13 23:35:17 -0300

    (frontend): Nos cambiamos al vue-cli

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,9 @@
-import 'materialize-css'
 import 'materialize-css/dist/css/materialize.css'
 import 'materialize-css/dist/js/materialize.js'
 
 import { createApp } from 'vue'
-
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).use(store).use(router).mount('#app')
