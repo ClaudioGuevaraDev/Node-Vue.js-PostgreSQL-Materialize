3faec1a2cf75ff784d6012ee01c6093b9724fafb
ClaudioGuevara <claudio.guevara.dev@gmail.com>
2022-01-14 12:06:46 -0300

    (backend): Registramos usuarios con password encriptada

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,13 +1,20 @@
 import { pool } from '../database'
+import {
+    encryptPassword
+} from '../libs/handlePassword'
 
 export const signUp = async (req, res) => {
-    const { password } = req.body
+    const { username, email, password } = req.body
 
     if (!password) return res.status(400).json({ message: 'Falta la contraseña.' })
 
-    
+    if (password.length < 8) return res.status(400).json({ message: 'La contraseña debe tener un largo mínimo de 8 caracteres.' })
 
-    res.send('vas bien')
+    const roleFound = await pool.query('SELECT * FROM roles WHERE name = $1', ['User'])
+
+    const { rows } = await pool.query('INSERT INTO users (username, email, password, roleId) VALUES ($1, $2, $3, $4) RETURNING *', [username, email, await encryptPassword(password), roleFound.rows[0].id])
+
+    res.status(201).json(rows[0])
 }
 
 export const signIn = async (req, res) => {
--- backend/src/controllers/users.js
+++ backend/src/controllers/users.js
@@ -1,7 +1,7 @@
 import { pool } from "../database"
 
 export const getAllUsers = async (req, res) => {
-    const { rows } = await pool.query('SELECT * FROM users')
+    const { rows } = await pool.query('SELECT U.id, U.username, U.email, U.password, U.verified, R.name as role FROM users as U JOIN roles as R ON R.id = U.roleId')
 
     res.json(rows)
 }
